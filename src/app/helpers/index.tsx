3f0495776e338055765022adc0c8069ca6b76b45
CynthiaNwa <c.nwankwo@ayahq.com>
2025-01-13 14:20:51 +0100

    Fixes: Error update

--- src/app/helpers/index.tsx
+++ src/app/helpers/index.tsx
@@ -2,5 +2,5 @@ import axios from "axios"
 
 
 export const postRequest =(url: string, body: any, config?: any)=> {
-    return axios.post(`${process.env.NEXT_PUBLIC_API_URL}${url}`, body, config)
+    return axios.post(`${process.env.NEXT_PUBLIC_API_URL}`, body, config)
 }
