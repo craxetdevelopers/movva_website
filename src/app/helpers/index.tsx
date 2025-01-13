53dff66e326238d168e4bf25d9af2cebe9c88fc3
CynthiaNwa <c.nwankwo@ayahq.com>
2025-01-13 15:41:48 +0100

    Fixes: Error update

--- src/app/helpers/index.tsx
+++ src/app/helpers/index.tsx
@@ -1,7 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 
-
-export const postRequest =(url: string, body: any)=> {
-    console.log(`${process.env.NEXT_PUBLIC_API_URL}`)
-    return axios.post(`${process.env.NEXT_PUBLIC_API_URL}${url}`, body)
-}
+export const postRequest = (url: string, body: any) => {
+  console.log(`${process.env.NEXT_PUBLIC_API_URL}`);
+  console.log("URL parameter received:", url); // Debug this
+  console.log("Full API URL:", `${process.env.NEXT_PUBLIC_API_URL}${url}`);
+  return axios.post(`${process.env.NEXT_PUBLIC_API_URL}${url}`, body);
+};
