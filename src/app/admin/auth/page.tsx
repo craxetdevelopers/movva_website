53d37bd3f814845c61285a774957f1bfb48c23fe
CynthiaNwa <c.nwankwo@ayahq.com>
2025-04-30 16:16:37 +0100

    Implemented the auth api and worked on the route guard

--- src/app/admin/auth/page.tsx
+++ src/app/admin/auth/page.tsx
@@ -1,102 +1,197 @@
-'use client'
+"use client";
 
 import {
-    Button,
-    FormControl,
-    Input,
-    InputGroup,
-    Stack,
-    Text,
-    VStack,
-  } from "@chakra-ui/react";
+  Box,
+  Button,
+  Center,
+  CircularProgress,
+  FormControl,
+  Input,
+  InputGroup,
+  Stack,
+  Text,
+  useToast,
+  VStack,
+} from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
-  import React from "react";
-  
-  const AdminSignin = () => {
-    const router = useRouter()
-    return (
-      <Stack
-        px={["1rem", "2rem", "2rem", "2rem", "0"]}
-        py={["1rem", "2rem", "3.5rem", "3.5rem"]}
-        bg={"#16193A"}
-        h={"100vh"}
+import React, { useEffect, useState } from "react";
+
+interface SigninPayload {
+  email: string;
+  password: string;
+}
+
+const AdminSignin = () => {
+  const toast = useToast();
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [formData, setFormData] = useState<SigninPayload>({
+    email: "",
+    password: "",
+  });
+
+  // Function that handles input change
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  // Function that handles submission of data
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      // Store token in local storage
+      if (data?.token?.token) {
+        localStorage.setItem("token", data?.token?.token);
+      }
+
+      if (data?.user?.type) {
+        localStorage.setItem("userType", data?.user?.type);
+      }
+      console.log(data?.token?.token);
+      // Check if the user is an admin
+      if (data?.user?.type == "admin") {
+        router.push("/admin");
+      } else {
+        throw new Error("Access denied: Not an admin");
+      }
+    } catch (err: any) {
+      toast({
+        title: "Signin Failed",
+        description: err.message || "Something went wrong",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const token = typeof window !== 'undefined'? localStorage.getItem("token") : null;
+    const userType = typeof window !== 'undefined'? localStorage.getItem("userType") : null;
+
+    if (token && userType === "admin") {
+      router.replace("/admin"); 
+    } else {
+      setIsLoggedIn(false)
+    }
+  }, []);
+
+  if (isLoggedIn) {
+      return (
+        <Center bg={'#16193A'} minH="100vh" flexDirection="column" gap={4}>
+          <CircularProgress
+            isIndeterminate
+            color="blue.400"
+            size="80px"
+            thickness="8px"
+          />
+          <Box fontWeight="medium" fontSize="lg">
+            Checking if Admin is Loggedin...
+          </Box>
+        </Center>
+      );
+    }
+
+  return (
+    <Stack
+      px={["1rem", "2rem", "2rem", "2rem", "0"]}
+      py={["1rem", "2rem", "3.5rem", "3.5rem"]}
+      bg={"#16193A"}
+      h={"100vh"}
+    >
+      <VStack
+        width="100%"
+        maxW={"1249px"}
+        gap={"60px"}
+        mx="auto"
+        justifyContent={"center"}
+        h={"100%"}
       >
         <VStack
-          width="100%"
-          maxW={"1249px"}
-          gap={"60px"}
-          mx="auto"
-          justifyContent={"center"}
-          h={"100%"}
+          w={"600px"}
+          border={"1px solid grey"}
+          px={"6rem"}
+          py={"4rem"}
+          borderRadius={"8px"}
         >
-          <VStack
-            w={"600px"}
-            border={"1px solid grey"}
-            px={"6rem"}
-            py={"4rem"}
-            borderRadius={"8px"}
-          >
-            <Text mb={"20px"} color={"#fff"} fontSize={"36px"}>
-              Admin
-            </Text>
-            <form style={{ width: "100%" }}>
-              <FormControl>
+          <Text mb={"20px"} color={"#fff"} fontSize={"36px"}>
+            Admin
+          </Text>
+          <form onSubmit={handleSubmit} style={{ width: "100%" }}>
+            <FormControl>
+              <Input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                color={"#000"}
+                placeholder="Email address*"
+                _placeholder={{ color: "rgb(180, 177, 177)" }}
+                bg={"#F9F9F9"}
+                borderRadius={"12px"}
+                p={"24px"}
+                border={"0.2px solid rgb(180, 177, 177)"}
+                isRequired
+              />
+              {/* <FormErrorMessage>{error.email}</FormErrorMessage> */}
+            </FormControl>
+            <FormControl mt={"30px"}>
+              <InputGroup>
                 <Input
-                  type="email"
-                  name="email"
-                  // value={formData.email}
-                  // onChange={handleInputChange}
-                  placeholder="Email address*"
+                  type="password"
+                  name="password"
+                  color={"#000"}
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  placeholder="Password*"
                   _placeholder={{ color: "rgb(180, 177, 177)" }}
                   bg={"#F9F9F9"}
                   borderRadius={"12px"}
                   p={"24px"}
                   border={"0.2px solid rgb(180, 177, 177)"}
+                  isRequired
                 />
-                {/* <FormErrorMessage>{error.email}</FormErrorMessage> */}
-              </FormControl>
-              <FormControl mt={"30px"}>
-                <InputGroup>
-                  <Input
-                    // type={show? 'text' : "password"}
-                    name="password"
-                    // value={formData.password}
-                    // onChange={handleInputChange}
-                    placeholder="Password*"
-                    _placeholder={{ color: "rgb(180, 177, 177)" }}
-                    bg={"#F9F9F9"}
-                    borderRadius={"12px"}
-                    p={"24px"}
-                    border={"0.2px solid rgb(180, 177, 177)"}
-                  />
-                  {/* <InputRightElement>
+                {/* <InputRightElement>
                                   <Box onClick={handleShowPassword} cursor={'pointer'}>
                                     {show ? <ViewOffIcon /> : <ViewIcon />}
                                   </Box>
                                 </InputRightElement> */}
-                </InputGroup>
-  
-                {/* <FormErrorMessage>{error.password}</FormErrorMessage> */}
-              </FormControl>
-              <VStack mt={'40px'}>
-                <Button
-                //   type="submit"
-                  // isLoading={signUpMutation.isPending}
-                  bg={"#17D1C6"}
-                  mr={3}
-                  p={"20px 50px"}
-                  // disabled={signUpMutation.isPending}
-                  onClick={() => router.push('/admin')}
-                >
-                  Signin
-                </Button>
-              </VStack>
-            </form>
-          </VStack>
+              </InputGroup>
+
+              {/* <FormErrorMessage>{error.password}</FormErrorMessage> */}
+            </FormControl>
+            <VStack mt={"40px"}>
+              <Button
+                type="submit"
+                isLoading={loading}
+                bg={"#17D1C6"}
+                mr={3}
+                p={"20px 50px"}
+                // disabled={signUpMutation.isPending}
+              >
+                Signin
+              </Button>
+            </VStack>
+          </form>
         </VStack>
-      </Stack>
-    );
-  };
-  
-  export default AdminSignin;
-  
+      </VStack>
+    </Stack>
+  );
+};
+
+export default AdminSignin;
