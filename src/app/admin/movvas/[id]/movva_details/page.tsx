a2bfe159c65348bef58bc913d1879b946b91a2e8
CynthiaNwa <c.nwankwo@ayahq.com>
2025-05-11 18:05:37 +0100

    Called the api for the admin movva table

--- src/app/admin/movvas/[id]/movva_details/page.tsx
+++ src/app/admin/movvas/[id]/movva_details/page.tsx
@@ -1,29 +1,35 @@
 // /app/admin/movvas/[id]/movva_details/page.tsx
 
-import DashboardLayout from '@/app/admin/DashboardLayout'
-import React from 'react'
-import AdminMovvaDetails from './components/AdminMovvaDetails'
+import DashboardLayout from "@/app/admin/DashboardLayout";
+import React from "react";
+import AdminMovvaDetails from "./components/AdminMovvaDetails";
+import axios from "axios";
 
 // Ensure params is handled correctly as per Next.js v15 changes
-export const dynamicParams = false;
+// export const dynamicParams = true;
 
 // No changes needed here for static generation
 export async function generateStaticParams() {
-  return [
-    { id: "1" },
-    { id: "2" },
-    { id: "3" },
-    { id: "4" },
-  ];
+  try {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/v1/users?page=1&limit=10&type=mover`
+    );
+    return res?.data?.data.map((movva: { id: number }) => ({
+      id: movva.id.toString(),
+    }));
+  } catch (error) {
+    console.error("Error in generateStaticParams:", error);
+    return [];
+  }
 }
 
-type tParams = Promise<{ id: string }>;
-
+// type tParams = Promise<{ id: string }>;
 
 // Awaiting params to handle the new structure in Next.js v15
-export default async function MovvaDetails({ params }: { params:  tParams }) {
+export default async function MovvaDetails({ params }: { params: { id: string } }) {
   // Await the params here
-  const { id }: {id: string} = await params;
+  const { id } = params;
+  // const { id }: { id: string } = await params;
 
   return (
     <DashboardLayout>
